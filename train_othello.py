594ccf774d4feff95ca4a21e69a3b954a3b801e0
ncridlig <ncridlig@ucsd.edu>
2023-02-25 12:49:57 -0800

    Implement Train Loop

--- train_othello.py
+++ train_othello.py
@@ -1,4 +1,8 @@
 import os.path
+import os
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
+import warnings
+warnings.filterwarnings('ignore')
 
 from tqdm import tqdm as progress_bar
 import argparse
@@ -11,18 +15,87 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 import torchvision.datasets as dset
 import torchvision.transforms as T
+import pdb
+from model import Conv8
+
+def compute_accuracy(logits, labels, args):
+
+    softmax_logits = F.softmax(logits, dim=1)
+    if args.sample_softmax:
+        predictions = torch.multinomial(softmax_logits, num_samples=1)
+    else:
+        predictions = torch.argmax(softmax_logits, dim=1) # list of batchsize
+
+    sorted_softmax = torch.argsort(softmax_logits, dim=1)
+
+    # Accuracy 1
+    num_correct = torch.sum(predictions == labels)
+    accuracy_1 = num_correct.item()
+
+    # Accuracy 3
+    sorted_confidence_3 = sorted_softmax[:,:3] # provides a batchsize x 3
+    top_3 = [labels[i] in sorted_confidence_3[i] for i in range(len(labels))]
+    accuracy_3 = sum(top_3)
+
+    # Accuracy 5
+    sorted_confidence_5 = sorted_softmax[:,:5] # provides a batchsize x 5
+    top_5 = [labels[i] in sorted_confidence_5[i] for i in range(len(labels))]
+    accuracy_5 = sum(top_5)
+
+    return predictions, [accuracy_1, accuracy_3, accuracy_5]
 
 def save_model(file_folder, model):
 
     path = os.path.join(file_folder, 'best_model.pth')
     torch.save(model.state_dict(), path)
 
-def normal_train(train_loader, val_loader, args):
+
+def validate_normal_model(val_loader, model, args, criterion):
+
+    """
+    Evaluates the model on a given dataset
+    """
+
+    model.eval()
+    losses = 0
+    running_accuracy = [0, 0, 0]
+    num_examples = 0
+    for step, (inputs, labels) in progress_bar(enumerate(val_loader), total=len(val_loader)):
+
+        if torch.cuda.is_available():
+            inputs, labels = inputs.cuda(), labels.cuda()
+        elif torch.backends.mps.is_available():
+            device = torch.device('mps')
+            inputs, labels = inputs.to(torch.float32), labels.to(torch.float32)
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+        
+        num_examples += inputs.size()[0]
+
+        logits = model(inputs)
+        prediction, batch_accuracies = compute_accuracy(logits, labels, args)
+        running_accuracy = [running_accuracy[i] + batch_accuracies[i] for i in range(len(running_accuracy))]
+        loss = criterion(logits, labels)
+        loss.backward()
+        losses += loss.item()
+
+    avg_accuracy = 100*running_accuracy/num_examples
+    avg_loss = losses/num_examples
+    return avg_accuracy, avg_loss
+
+
+
+
+
+
+
+def normal_train(model, train_loader, val_loader, args):
     """
     Performs training without SWA
     :param train_loader: training dataset
     :param val_loader: validation dataset
     """
+
 
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
@@ -31,27 +104,40 @@ def normal_train(train_loader, val_loader, args):
     for epoch_count in range(epochs):
         losses = 0
         model.train()
-
+        running_accuracy = [0, 0, 0]
+        num_examples = 0
         for step, (inputs, labels) in progress_bar(enumerate(train_loader), total=len(train_loader)):
 
             if torch.cuda.is_available():
                 inputs, labels = inputs.cuda(), labels.cuda()
+            elif torch.backends.mps.is_available():
+                device = torch.device('mps')
+                inputs, labels = inputs.to(torch.float32), labels.to(torch.float32)
+                inputs, labels = inputs.to(device), labels.to(device)
+            
+            num_examples += inputs.size()[0]
 
             logits = model(inputs)
-            softmax_logits = F.softmax(logits, dim=1)
-            predictions = torch.argmax(softmax_logits, dim=1)
+            prediction, batch_accuracies = compute_accuracy(logits, labels, args)
+            running_accuracy = [running_accuracy[i] + batch_accuracies[i] for i in range(len(running_accuracy))]
+            labels = labels.to(torch.int32)
             loss = criterion(logits, labels)
             loss.backward()
-
             optimizer.step()  # backprop to update the weights
-            model.scheduler.step()  # Update learning rate schedule
             optimizer.zero_grad()
             losses += loss.item()
-            print('yo')
 
-        # Run on validation set
-        run_eval(args, model, datasets, tokenizer, split='validation')
-        print('epoch', epoch_count, '| losses:', losses)
+        avg_accuracy = 100*running_accuracy/num_examples
+        avg_loss = losses/num_examples
+        val_accuracy, val_loss = validate_normal_model(val_loader, model, args, criterion)
+        print('----------------------------  Epoch ' + str(epoch_count) + ' ----------------------------')
+        print('Training Loss: ' + avg_loss)
+        print('Training Accuracies: ' + avg_accuracy)
+        print('Validation Loss: ' + val_loss)
+        print('Validation Accuracies: ' + val_accuracy)
+        print('-----------------------------------------------------------------')
+
+    
 
 
 def params():
@@ -60,18 +146,20 @@ def params():
     :return: argparser
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch-size", default=1, type=int,
-                        help="Batch size per GPU/CPU for training and evaluation.")
     parser.add_argument("--learning-rate", default=1e-3, type=float,
                         help="Model learning rate starting point.")
-    parser.add_argument("--batch-size", default=1, type=int,
+    parser.add_argument("--batch-size", default=1024, type=int,
                         help="Batch size per GPU/CPU for training and evaluation.")
     parser.add_argument("--weight-decay", default=1e-8, type=float,
                         help="L2 Regularization")
+    parser.add_argument("--epochs", default=100, type=int,
+                        help="Number of epochs to train for")
     parser.add_argument("--swa-enabled", action='store_true',
                         help="Enables Stochastic Weighting Average")
-    parser.parse_args()
-    return parser
+    parser.add_argument("--sample-softmax", action='store_true',
+                        help="Samples from Move Probability Distribution")
+    args = parser.parse_args()
+    return args
 
 def load_data():
 
@@ -105,6 +193,7 @@ def load_data():
 
 if __name__ == '__main__':
 
+
     args = params()
     train_dataset, val_dataset, test_dataset = load_data()
 
@@ -114,9 +203,13 @@ if __name__ == '__main__':
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=True)
 
     # Initialize Othello model
-    model = None
+    model = Conv8()
 
     if torch.cuda.is_available():
         model = model.cuda()
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+        model = model.to(device)
 
+    normal_train(model=model, train_loader=train_loader, val_loader=val_loader, args=args)
 
