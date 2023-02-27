d166ed307a2dfef9f7eb72efa3166158e13a2be0
Akshay Gopalkrishnan <agopalkr@UCSD.EDU>
2023-02-26 19:06:11 -0800

    Training process finished

--- train_othello.py
+++ train_othello.py
@@ -1,32 +1,32 @@
 import os.path
 import os
+import pdb
+
+import pandas as pd
+
 os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
 import warnings
 warnings.filterwarnings('ignore')
 
 from tqdm import tqdm as progress_bar
 import argparse
-from torch.cuda import is_available
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
-import torchvision.datasets as dset
-import torchvision.transforms as T
-import pdb
 from model import Conv8
+from copy import deepcopy
+import time
 
 def compute_accuracy(logits, labels, args):
 
     softmax_logits = F.softmax(logits, dim=1)
     if args.sample_softmax:
-        predictions = torch.multinomial(softmax_logits, num_samples=1)
+        predictions = torch.multinomial(softmax_logits, num_samples=1).flatten()
     else:
         predictions = torch.argmax(softmax_logits, dim=1) # list of batchsize
 
-    sorted_softmax = torch.argsort(softmax_logits, dim=1)
+    sorted_softmax = torch.argsort(softmax_logits, dim=1, descending=True)
 
     # Accuracy 1
     num_correct = torch.sum(predictions == labels)
@@ -46,8 +46,9 @@ def compute_accuracy(logits, labels, args):
 
 def save_model(file_folder, model):
 
-    path = os.path.join(file_folder, 'best_model.pth')
-    torch.save(model.state_dict(), path)
+    # Save the model into the designated folder
+    path = os.path.join(file_folder, timestr + '.pth')
+    torch.save(model, path)
 
 
 def validate_normal_model(val_loader, model, args, criterion):
@@ -64,17 +65,17 @@ def validate_normal_model(val_loader, model, args, criterion):
 
         if torch.cuda.is_available():
             inputs, labels = inputs.cuda(), labels.cuda()
+            inputs, labels = inputs.float(), labels.float()
         elif torch.backends.mps.is_available():
             device = torch.device('mps')
             inputs, labels = inputs.to(torch.float32), labels.to(torch.float32)
             inputs, labels = inputs.to(device), labels.to(device)
-            
-        
-        num_examples += inputs.size()[0]
 
+        num_examples += inputs.size()[0]
         logits = model(inputs)
         prediction, batch_accuracies = compute_accuracy(logits, labels, args)
         running_accuracy = [running_accuracy[i] + batch_accuracies[i] for i in range(len(running_accuracy))]
+        labels = labels.to(torch.int64)
         loss = criterion(logits, labels)
         loss.backward()
         losses += loss.item()
@@ -84,11 +85,6 @@ def validate_normal_model(val_loader, model, args, criterion):
     return avg_accuracy, avg_loss
 
 
-
-
-
-
-
 def normal_train(model, train_loader, val_loader, args):
     """
     Performs training without SWA
@@ -96,12 +92,19 @@ def normal_train(model, train_loader, val_loader, args):
     :param val_loader: validation dataset
     """
 
-
+    # Initialize the cross entropy and Adam optimizer to update weights
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
     epochs = args.epochs
 
+    # Used to save the best model
+    best_valid_accuracy = [0.0, 0.0, 0.0]
+    best_training_accuracy = [0.0, 0.0, 0.0]
+    best_model = None
+
     for epoch_count in range(epochs):
+
+        # Initializing running accuracy and loss statistics
         losses = 0
         model.train()
         running_accuracy = [0, 0, 0]
@@ -110,17 +113,18 @@ def normal_train(model, train_loader, val_loader, args):
 
             if torch.cuda.is_available():
                 inputs, labels = inputs.cuda(), labels.cuda()
+                inputs, labels = inputs.float(), labels.float()
+
             elif torch.backends.mps.is_available():
                 device = torch.device('mps')
                 inputs, labels = inputs.to(torch.float32), labels.to(torch.float32)
                 inputs, labels = inputs.to(device), labels.to(device)
             
             num_examples += inputs.size()[0]
-
             logits = model(inputs)
             prediction, batch_accuracies = compute_accuracy(logits, labels, args)
             running_accuracy = [running_accuracy[i] + batch_accuracies[i] for i in range(len(running_accuracy))]
-            labels = labels.to(torch.int32)
+            labels = labels.to(torch.int64)
             loss = criterion(logits, labels)
             loss.backward()
             optimizer.step()  # backprop to update the weights
@@ -130,14 +134,33 @@ def normal_train(model, train_loader, val_loader, args):
         avg_accuracy = [100 * running_accuracy[i] / num_examples for i in range(len(running_accuracy))]
         avg_loss = losses/num_examples
         val_accuracy, val_loss = validate_normal_model(val_loader, model, args, criterion)
+        print()
         print('----------------------------  Epoch ' + str(epoch_count) + ' ----------------------------')
-        print('Training Loss: ' + avg_loss)
-        print('Training Accuracies: ' + avg_accuracy)
-        print('Validation Loss: ' + val_loss)
-        print('Validation Accuracies: ' + val_accuracy)
-        print('-----------------------------------------------------------------')
+        print('Training Loss: ' + str(avg_loss))
+        print('Training Accuracies: ' + str(avg_accuracy))
+        print('Validation Loss: ' + str(val_loss))
+        print('Validation Accuracies: ' + str(val_accuracy))
+        print('------------------------------------------------------------------')
 
-    
+        # Update the model if it achieves a higher
+        # accuracy then the previous model
+        if val_accuracy[0] > best_valid_accuracy[0]:
+            best_valid_accuracy[0] = val_accuracy[0]
+            best_model = deepcopy(model.state_dict())
+
+        # Update calidation accuracy statistics
+        best_valid_accuracy[1] = max(val_accuracy[1], best_valid_accuracy[1])
+        best_valid_accuracy[2] = max(val_accuracy[2], best_valid_accuracy[2])
+
+        # Update training accuracy statistics
+        best_training_accuracy[0] = max(avg_accuracy[0], best_training_accuracy[0])
+        best_training_accuracy[1] = max(avg_accuracy[1], best_training_accuracy[1])
+        best_training_accuracy[2] = max(avg_accuracy[2], best_training_accuracy[2])
+
+
+    # Save the best model from training
+    save_model('models', best_model)
+    return best_training_accuracy, best_valid_accuracy
 
 
 def params():
@@ -146,13 +169,13 @@ def params():
     :return: argparser
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--learning-rate", default=1e-3, type=float,
+    parser.add_argument("--learning-rate", default=5e-4, type=float,
                         help="Model learning rate starting point.")
-    parser.add_argument("--batch-size", default=1024, type=int,
+    parser.add_argument("--batch-size", default=4096, type=int,
                         help="Batch size per GPU/CPU for training and evaluation.")
-    parser.add_argument("--weight-decay", default=1e-8, type=float,
+    parser.add_argument("--weight-decay", default=1e-4 , type=float,
                         help="L2 Regularization")
-    parser.add_argument("--epochs", default=100, type=int,
+    parser.add_argument("--epochs", default=8, type=int,
                         help="Number of epochs to train for")
     parser.add_argument("--swa-enabled", action='store_true',
                         help="Enables Stochastic Weighting Average")
@@ -160,6 +183,42 @@ def params():
                         help="Samples from Move Probability Distribution")
     args = parser.parse_args()
     return args
+
+def save_experiment(args, statistics):
+    """
+    Saves the experiment results to a csv
+    :param args: The hyperparameters used
+    :param statistics: The accuracies for the training, validation, and test sets
+    """
+    trial_dict = {
+        'Model name': [timestr],
+        'Learning rate': [args.learning_rate],
+        'Batch size': [args.batch_size],
+        'Weight decay': [args.weight_decay],
+        'Epochs': [args.epochs],
+        'Stochastic Weighting Averaging': [args.swa_enabled],
+        'Softmax Sampling': [args.sample_softmax],
+        'Max Training Accuracy': [statistics[0][0]],
+        'Max Training Accuracy3': [statistics[0][1]],
+        'Max Training Accuracy5': [statistics[0][2]],
+        'Max Validation Accuracy': [statistics[1][0]],
+        'Max Validation Accuracy3': [statistics[1][1]],
+        'Max Validation Accuracy5': [statistics[1][2]],
+        'Testing Accuracy': [statistics[2][0]],
+        'Testing Accuracy3': [statistics[2][1]],
+        'Testing Accuracy5': [statistics[2][2]],
+        'Testing Loss': [statistics[3]]
+    }
+
+    trial_dict = pd.DataFrame(trial_dict)
+    need_header = not os.path.exists('results.csv')
+
+    if need_header:
+        trial_dict.to_csv('results.csv', index=False, header=need_header)
+    else:
+        trial_dict.to_csv('results.csv', mode='a', index=False, header=need_header)
+
+
 
 def load_data():
 
@@ -193,7 +252,7 @@ def load_data():
 
 if __name__ == '__main__':
 
-
+    # Load the hyperparameters and the dataset
     args = params()
     train_dataset, val_dataset, test_dataset = load_data()
 
@@ -204,12 +263,31 @@ if __name__ == '__main__':
 
     # Initialize Othello model
     model = Conv8()
+    timestr = time.strftime("%Y%m%d-%H%M%S")
 
+    # Convert to mps or cuda
     if torch.cuda.is_available():
         model = model.cuda()
     elif torch.backends.mps.is_available():
         device = torch.device('mps')
         model = model.to(device)
 
-    normal_train(model=model, train_loader=train_loader, val_loader=val_loader, args=args)
+    # Training the model on the training set
+    best_training_acc, best_val_acc = normal_train(model=model, train_loader=train_loader,
+                                                   val_loader=val_loader, args=args)
+
+    # Load the best model and evaluate it on the test set
+    best_model = Conv8()
+    best_model.load_state_dict(torch.load(os.path.join('models', timestr, 'best_model.pth')))
+
+    if torch.cuda.is_available():
+        best_model = best_model.cuda()
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+        best_model = best_model.to(device)
+
+    test_accuracy, test_loss = validate_normal_model(val_loader=test_loader, model=best_model,args=args,
+                                         criterion=torch.nn.CrossEntropyLoss())
+    statistics = [best_training_acc, best_val_acc, test_accuracy, test_loss]
+    save_experiment(args, statistics)
 
